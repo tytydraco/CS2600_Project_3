577973e442a4b8af080f4bfecbed0deed1682553
Tyler <tylernij@gmail.com>
2021-10-23 19:22:26 -0700

    Clear screen for Mac too

--- address_book_menu.c
+++ address_book_menu.c
@@ -137,11 +137,11 @@ void menu_header(const char *str)
 {
 	fflush(stdout);
 
-	#ifdef __linux__
-		system("clear");
-	#else
+#ifndef __linux__
 		system("cls");
-	#endif
+#else
+		system("clear");
+#endif
 
 	printf("#######  Address Book  #######\n");
 	if (*str != '\0')
